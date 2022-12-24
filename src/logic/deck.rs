efdfeb455dce34857d703042f4c5444281da7782
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-24 20:27:32 +0200

    added deck

--- src/logic/deck.rs
+++ src/logic/deck.rs
@@ -1,3 +1,9 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(EnumIter, Copy, Clone, Debug)]
 pub enum Suit {
     Spades,
     Clubs,
@@ -17,6 +23,27 @@ impl Suit {
 }
 
 pub struct Card {
-    value: i32,
-    suit: Suit
+    pub value: i32,
+    pub suit: Suit
+}
+
+pub struct Deck {
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn new() -> Deck {
+        let mut deck = Deck { cards: vec![] };
+        deck.initialize_deck();
+        deck
+    }
+
+    pub fn initialize_deck(&mut self) {
+        for suit in Suit::iter() {
+            for value in 2..14 {
+                self.cards.push(Card { value, suit })
+            }
+        }
+        self.cards.shuffle(&mut thread_rng());
+    }
 }
