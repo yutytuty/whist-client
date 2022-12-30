668e88ed04737504cc7b2d813af149a1b45586e1
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-30 13:58:36 +0200

    Add getters, constructor, player card methods and reformat

--- src/logic/player.rs
+++ src/logic/player.rs
@@ -1,5 +1,5 @@
-use crate::logic::table::{Card, Deck, Table};
 use crate::logic::error::LogicError;
+use crate::logic::table::{Card, Deck, Table};
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
 pub struct Player {
@@ -9,7 +9,10 @@ pub struct Player {
 
 impl Player {
     pub fn new(name: &str) -> Player {
-        Player { name: name.to_string(), hand: vec![] }
+        Player {
+            name: name.to_string(),
+            hand: vec![],
+        }
     }
 
     pub fn draw(&mut self, deck: &mut Deck) -> Option<Card> {
@@ -18,37 +21,57 @@ impl Player {
             Some(card)
         } else {
             None
-        }
+        };
     }
 
-    pub fn play_card(&mut self, table: &mut Table, card: Card) {
+    pub fn play_card_from_hand(&mut self, table: &mut Table, index: usize) {
+        table.place_card(self.clone(), self.hand.remove(index));
+    }
+
+    pub fn play_card(&self, table: &mut Table, card: Card) {
         table.place_card(self.clone(), card);
+    }
+
+    pub fn get_hand(&self) -> Vec<Card> {
+        self.hand.clone()
     }
 }
 
 #[cfg(test)]
 pub mod tests {
-    use std::collections::HashMap;
-    use lazy_static::lazy_static;
-    use crate::logic::table::Suit;
     use super::*;
+    use crate::logic::table::Suit;
+    use lazy_static::lazy_static;
+    use std::collections::HashMap;
 
     lazy_static! {
         pub static ref TEST_PLAYER_1: Player = Player {
             name: "Alice".to_string(),
-            hand: vec![Card { value: 5, suit: Suit::Spades }],
+            hand: vec![Card {
+                value: 5,
+                suit: Suit::Spades
+            }],
         };
         pub static ref TEST_PLAYER_2: Player = Player {
             name: String::from("Bob"),
-            hand: vec![Card { value: 5, suit: Suit::Hearts }],
+            hand: vec![Card {
+                value: 5,
+                suit: Suit::Hearts
+            }],
         };
         pub static ref TEST_PLAYER_3: Player = Player {
             name: String::from("Charlie"),
-            hand: vec![Card { value: 6, suit: Suit::Spades }],
+            hand: vec![Card {
+                value: 6,
+                suit: Suit::Spades
+            }],
         };
         pub static ref TEST_PLAYER_4: Player = Player {
             name: String::from("Dave"),
-            hand: vec![Card { value: 4, suit: Suit::Spades }],
+            hand: vec![Card {
+                value: 4,
+                suit: Suit::Spades
+            }],
         };
     }
 }
