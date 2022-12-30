9b0e697ab2a3bdfdab7d0ab5d25ff98742298582
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-30 23:12:13 +0200

    Move shared logic between client and server to https://github.com/yutytuty/WhistCommon

--- src/logic/error.rs
+++
@@ -1,5 +0,0 @@
-#[derive(thiserror::Error, Debug)]
-pub enum LogicError {
-    #[error("Generic logic error: {0}")]
-    Generic(String),
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-#![allow(unused)]
-extern crate core;
-
-mod logic;
-mod packet;
-
 fn main() {
     println!("Hello");
 }
